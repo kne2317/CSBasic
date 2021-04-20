f83e14af2c48ce9acd6116e249824a2041b80d14
kne <s2019s06@e-mirim.hs.kr>
2021-04-21 08:51:20 +0900

    12-3

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -10,6 +10,10 @@ namespace CSBasic
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Hello C# Programming!");
+
+            
             Console.WriteLine(52 + 273);
             Console.WriteLine(5 + 3 * 2);
 
@@ -23,6 +27,28 @@ namespace CSBasic
             Console.WriteLine(1%2);
 
             Console.WriteLine(4 % 3);
+            Console.WriteLine(-4 % 3);
+            Console.WriteLine(4 % -3);
+            Console.WriteLine(-4 % -3);
+
+
+            Console.WriteLine(52.273);
+
+            Console.WriteLine(0);
+            Console.WriteLine(0.0);
+
+            Console.WriteLine(1.0 + 2.0);
+
+            Console.WriteLine('A');
+
+            Console.WriteLine("안녕하세요");
+
+            Console.WriteLine("한빛\t아카데미");
+            Console.WriteLine("한빛\n아카데미");
+            Console.WriteLine("\"\"\"");
+
+            Console.WriteLine("안녕하세요"[0]);
+
 
             Console.WriteLine("A" + "B");
             Console.WriteLine("A" + 1);
