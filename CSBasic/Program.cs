763bd9ebcb711650638b5c3f0f7fc6bb437394b9
kne <s2019s06@e-mirim.hs.kr>
2021-04-21 08:27:44 +0900

    초기 커밋.

---
+++ CSBasic/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(52 + 273);
+            Console.WriteLine(5 + 3 * 2);
+
+            Console.WriteLine(10 % 5);
+            Console.WriteLine(7 & 3);
+
+            Console.WriteLine(1 + 2);
+            Console.WriteLine(1-2);
+            Console.WriteLine(1*2);
+            Console.WriteLine(1/2);
+            Console.WriteLine(1%2);
+
+            Console.WriteLine(4 % 3);
+
+            Console.WriteLine("A" + "B");
+            Console.WriteLine("A" + 1);
+            Console.WriteLine(1 + "A");
+            Console.WriteLine("1" + "1");
+
+        }
+    }
+}
