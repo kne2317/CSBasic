55170b25b26c2adc65d29c42f6b62916e32a901d
kne <s2019s06@e-mirim.hs.kr>
2021-05-26 09:11:18 +0900

    asdf

--- Cbasic4/Program.cs
+++ Cbasic4/Program.cs
@@ -10,14 +10,32 @@ namespace Cbasic4
     {
         static void Main(string[] args)
         {
-            Car[] cars = new Car[10];
+            Car cars = new Car();
+            cars.SetInTime();
+            cars.SetOutTime();
+
+            Random r = new Random();
+            Console.WriteLine(r.NextDouble());
+            Console.WriteLine(r.NextDouble());
+
+
         }
     }
     class Car
     {
         int carNumber;
-        int inTime;
-        int outTime;
+        DateTime inTime;
+        DateTime outTime;
+
+        public void SetInTime()
+        {
+            this.inTime = DateTime.Now;
+
+        }
+        public void SetOutTime()
+        {
+            this.outTime = DateTime.Now;
+        }
     }
 
 }
